b14078df569ad26a7040b2849f945ed20010f828
roxanamoisa <roxanamoisa22@gmail.com>
2018-03-29 19:32:25 +0300

    almost A.2

--- app/src/main/java/practicaltest01var02/eim/systems/cs/pub/ro/practicaltest01var02/PracticalTest01Var02MainActivity.java
+++ app/src/main/java/practicaltest01var02/eim/systems/cs/pub/ro/practicaltest01var02/PracticalTest01Var02MainActivity.java
@@ -8,45 +8,44 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import static practicaltest01var02.eim.systems.cs.pub.ro.practicaltest01var02.R.id.left_button;
+import static practicaltest01var02.eim.systems.cs.pub.ro.practicaltest01var02.R.id.left_edit_text;
+import static practicaltest01var02.eim.systems.cs.pub.ro.practicaltest01var02.R.id.right_button;
 
 public class PracticalTest01Var02MainActivity extends AppCompatActivity {
+
+    EditText firstNumber;
+    EditText secondNumber;
+    TextView addResult;
+    Button btnAdd;
+
+    double num1,num2,sum;
+
+    private ButtonClickListener buttonClickListener = new ButtonClickListener();
+    private class ButtonClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            num1 = Double.parseDouble(firstNumber.getText().toString());
+            num2 = Double.parseDouble(secondNumber.getText().toString());
+            sum = num1 + num2;
+            addResult.setText(Double.toString(sum));
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practical_test01_var02_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        firstNumber = (EditText)findViewById(R.id.left_edit_text);
+        secondNumber = (EditText)findViewById(R.id.right_edit_text);
+        addResult = (TextView)findViewById(R.id.left_nonedit_text);
+        btnAdd = (Button)findViewById(R.id.left_button);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_practical_test01_var02_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
